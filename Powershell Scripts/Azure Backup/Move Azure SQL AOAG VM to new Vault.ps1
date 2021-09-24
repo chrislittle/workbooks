1f22ef9d4a4cbcf9abcfdca202aa75066f51565e
chrislittle <chris@cloudarchitect.email>
2021-09-24 10:32:52 -0400

    added sql cluster script

--- Powershell Scripts/Azure Backup/Move Azure SQL AOAG VM to new Vault.ps1
+++ Powershell Scripts/Azure Backup/Move Azure SQL AOAG VM to new Vault.ps1
@@ -16,6 +16,8 @@ $newrg = "NEW RESOURCE GROUP"
 $currentvault = Get-AzRecoveryServicesVault -ResourceGroupName $currentrg -Name "CURRENT VAULT NAME"
 $newvault = Get-AzRecoveryServicesVault -ResourceGroupName $newrg -Name "NEW VAULT NAME"
 $vmname = "VM NAME"
+# HIDDEN_RESOURCE_GROUP_FOR_RESTORE_POINT_COLLECTIONS
+$restorePointCollectionrg = "restorePointCollection rg name"
 
 # set the recovery context to the existing vault
 Set-AzRecoveryServicesVaultContext -Vault $currentvault
@@ -26,22 +28,22 @@ $currentitems = Get-AzRecoveryServicesBackupItem -Container $container -Workload
 foreach($currentitem in $currentitems){
 Disable-AzRecoveryServicesBackupProtection -Item $currentitem -Force}
 
-# disable protection for MSSQL Type (cluster AG name required for clusters
+# Get the container & backup items details and disable protection for MSSQL Cluster databases
 # NOTE: AFTER MOVING PRIMARY CLUSTER NODE ADDITIONAL SECONDARY NODES DO NOT NEED TO RUN THE MS SQL DISABLE PROTECTION AS THIS WAS ALREADY DONE VIA PRIMARY NODE ON THE CLUSTER AG
-$currentsqlitems = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureWorkload -WorkloadType MSSQL -Name "availability group name or sql standalone vm name "
+$currentsqlitems = Get-AzRecoveryServicesBackupItem -BackupManagementType AzureWorkload -WorkloadType MSSQL -Name "availability group name"
 foreach($currentsqlitem in $currentsqlitems){
 Disable-AzRecoveryServicesBackupProtection -Item $currentsqlitem -Force}
 
 # remove restore point collections (snapshots)
-$restorePointCollection = Get-AzResource -ResourceGroupName HIDDEN_RESOURCE_GROUP_FOR_RESTORE_POINT_COLLECTIONS -name *MANUALLY TYPE IN VM NAME KEEP STARS* -ResourceType Microsoft.Compute/restorePointCollections
+$restorePointCollection = Get-AzResource -ResourceGroupName $restorePointCollectionrg -name *MANUALLY TYPE IN VM NAME KEEP STARS* -ResourceType Microsoft.Compute/restorePointCollections
 $restorePointCollection | FT
-Read-Host -Prompt "Confirm the restorePointCollection list matches your intent, Press any key to continue"
+Read-Host -Prompt "Confirm the restorePointCollection list matches your intent, Press enter key to continue"
 Remove-AzResource -ResourceId $restorePointCollection.ResourceId -Force
 
 # Move resources associated with VM (NIC, NSG, DISK)
 $resources = Get-AzResource -ResourceGroupName $currentrg -name *MANUALLY TYPE IN VM NAME KEEP STARS*
 $resources | FT
-Read-Host -Prompt "Confirm the list of resources to move match your intent, Press any key to continue"
+Read-Host -Prompt "Confirm the list of resources to move match your intent, Press enter key to continue"
 Move-AzResource -DestinationResourceGroupName $newrg -ResourceId $Resources.ResourceId -Force
 
 # set the recovery context to the new vault
@@ -51,13 +53,13 @@ Set-AzRecoveryServicesVaultContext -Vault $newvault
 $vmpolicy = Get-AzRecoveryServicesBackupProtectionPolicy -Name "POLICY NAME IN NEW VAULT"
 $sqlpolicy = Get-AzRecoveryServicesBackupProtectionPolicy -Name "POLICY NAME IN NEW VAULT"
 
-# enable protection of the newly moved VM & SQL databases to the policy defined. OS Disks only for SQL on IaaS VMs, remove -ExcludeAllDataDisks if required
-Enable-AzRecoveryServicesBackupProtection -Policy $vmpolicy -ExcludeAllDataDisks -Name $vmname -ResourceGroupName $newrg -VaultId $newvault.ID
+# enable protection of the newly moved VM & SQL databases to the policy defined.
+Enable-AzRecoveryServicesBackupProtection -Policy $vmpolicy -Name $vmname -ResourceGroupName $newrg -VaultId $newvault.ID
 $vmdetails = Get-AzResource -Name $vmname -ResourceType Microsoft.Compute/virtualMachines
 Register-AzRecoveryServicesBackupContainer -ResourceId $vmdetails.resourceid -BackupManagementType AzureWorkload -WorkloadType MSSQL -VaultId $newvault.ID -Force
 
 # NOTE: AFTER MOVING PRIMARY CLUSTER NODE ADDITIONAL SECONDARY NODES DO NOT NEED TO ENABLE CLUSTER DB PROTECTION AS THIS WAS ALREADY DONE VIA PRIMARY NODE ON THE CLUSTER AG
 Get-AzRecoveryServicesBackupProtectableItem -WorkloadType MSSQL -VaultId $newvault.ID
-Read-Host -Prompt "confirm the item you wish to protect is present, Press any key to continue"
+Read-Host -Prompt "confirm the item you wish to protect is present, Press enter key to continue"
 $sqldatabase = Get-AzRecoveryServicesBackupProtectableItem -workloadType MSSQL -ItemType SQLDataBase -VaultId $newvault.ID -ServerName FQDN of Cluster AG or VM Name
 Enable-AzRecoveryServicesBackupProtection -ProtectableItem $sqldatabase -Policy $sqlpolicy
--- Powershell Scripts/Azure Backup/README.md
+++ Powershell Scripts/Azure Backup/README.md
@@ -3,5 +3,5 @@
 SAMPLE POWERSHELL SCRIPTS FOR MOVING MSSQL & AzureVM Workload Types to new Vaults
 
 * Move Azure VM to new vault.ps1 - Moves (while retaining existing data) an Azure VM to a new recovery services vault. This script is not for use with workloads being protected such as MSSQL.
-* Move Azure SQL Standalone VM to new Vault.ps1 - Moves (while retaining existing data) a stand alone MS SQL Azure VM to a new recovery services vault. This script is capable of migrating both Azure VM backup and MS SQL database configurations.
-
+* Move Azure SQL Standalone VM to new Vault.ps1 - Moves (while retaining existing data) a stand alone MS SQL Azure VM to a new recovery services vault. This script is capable of moving both Azure VM backup and MS SQL database configurations.
+* Move Azure SQL AOAG VM to new Vault.ps1 - Moves (while retaining existing data) MS SQL Always On Availability Group databases & Azure VM to a new recovery services vault. This script is capable of moving both Azure VM backup and MS SQL Cluster (AOAG) database configurations. Each node in the cluster needs the script run but please note there are some components of the secondary nodes that can be ommitted from the script.
