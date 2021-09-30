e8b1dffe6ab95505df0c17f79b5019a1112065e1
chrislittle <chris@cloudarchitect.email>
2021-09-30 15:34:28 -0400

    added encryption at host script framework

---
+++ Powershell Scripts/Encryption At Host/encryption at host.ps1
@@ -0,0 +1,18 @@
+﻿# Initial Azure Encryption at Host script. Enables Encryption at Host on VM & updates the OS & Data Disks to SSE with CMK to the Disk Encryption Set
+
+$ResourceGroupName = "newbackup"
+$VMName = "sqlstandalone"
+$diskEncryptionSetName = "des-sqlnodes"
+$VM = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName
+Stop-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -Force
+$vmosdisk = $vm.StorageProfile.OsDisk
+$vmdatadisks = $vm.StorageProfile.DataDisks
+$diskEncryptionSet = Get-AzDiskEncryptionSet -ResourceGroupName $ResourceGroupName -Name $diskEncryptionSetName
+Update-AzVM -VM $VM -ResourceGroupName $ResourceGroupName -EncryptionAtHost $true
+
+# Update OS disk to SSE with CMK
+New-AzDiskUpdateConfig -EncryptionType "EncryptionAtRestWithCustomerKey" -DiskEncryptionSetId $diskEncryptionSet.Id | Update-AzDisk -ResourceGroupName $ResourceGroupName -DiskName $vmosdisk.name
+
+# Update Data disks to SSE with CMK
+foreach($vmdatadisk in $vmdatadisks){
+New-AzDiskUpdateConfig -EncryptionType "EncryptionAtRestWithCustomerKey" -DiskEncryptionSetId $diskEncryptionSet.Id | Update-AzDisk -ResourceGroupName $ResourceGroupName -DiskName $vmdatadisk.name}
